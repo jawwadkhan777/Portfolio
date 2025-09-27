e18c416244a1d46f731d49f47aa905959bcc37a0
Muhammad Jawwad Khan <m.jawwadkhan777@gmail.com>
2025-09-27 23:44:44 +0500

    change on button text

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -101,12 +101,12 @@ const Header = () => {
         <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
             <motion.a 
             initial={{y:30, opacity:0}} whileInView={{y:0, opacity:1}} transition={{duration:0.6, delay:1}}
-            href="#contact" className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent'>contact me
+            href="#contact" className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent'>Contact Me
                 <Image src={assets.right_arrow_white} alt='' className='w-4' />
             </motion.a>
             <motion.a 
             initial={{y:30, opacity:0}} whileInView={{y:0, opacity:1}} transition={{duration:0.6, delay:1.2}}
-            href="/sample-resume.pdf" className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>my resume
+            href="/sample-resume.pdf" className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>My Resume
                 <Image src={assets.download_icon} alt='' className='w-4' />
             </motion.a>
         </div>
