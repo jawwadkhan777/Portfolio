90c2dad72897c5b8c762afd60e917bc2af87e166
Muhammad Jawwad Khan <m.jawwadkhan777@gmail.com>
2025-10-02 14:13:28 +0500

    Added some changes and corrected resume path

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -106,7 +106,7 @@ const Header = () => {
             </motion.a>
             <motion.a 
             initial={{y:30, opacity:0}} whileInView={{y:0, opacity:1}} transition={{duration:0.6, delay:1.2}}
-            href="/sample-resume.pdf" className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>My Resume
+            href="/JawwadKhan_CS_resume.pdf" className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>My Resume
                 <Image src={assets.download_icon} alt='' className='w-4' />
             </motion.a>
         </div>
